d028ab00172b56ec038991ad2b1aa6059301bf4d
pkindalov <panayotkindalov@gmail.com>
2024-04-02 17:01:01 +0300

    Added tsconfig.json and the first task from the lab

---
+++ IntroductionToTS/src/CalculateRectangleArea/calcRectArea.ts
@@ -0,0 +1,8 @@
+/* Write a simple function that calculates rectangle area. You will be given two integers (whole) numbers, 
+   which will represent length and width of the rectangle. Calculate and print the area of the rectangle.
+*/
+
+const calcRectArea = (length: number, whidth: number):number => length * whidth;
+
+console.log(calcRectArea(5, 7));
+console.log(calcRectArea(6, 8));
