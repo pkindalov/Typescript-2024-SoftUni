84d7d190bf001c3bf1e2a8ce75aed38f6dc33e9e
pkindalov <panayotkindalov@gmail.com>
2024-04-02 17:11:32 +0300

    Added task find Large Number

---
+++ IntroductionToTS/dist/LargestNumber/findLargestNum.js
@@ -0,0 +1,5 @@
+"use strict";
+const findLargestNum = (num1, num2, num3) => Math.max(num1, num2, num3);
+console.log(findLargestNum(5, -3, 16));
+console.log(findLargestNum(-3, -5, -22.5));
+//# sourceMappingURL=findLargestNum.js.map
---
+++ IntroductionToTS/src/LargestNumber/findLargestNum.ts
@@ -0,0 +1,4 @@
+const findLargestNum = (num1: number, num2: number, num3: number): number => Math.max(num1, num2, num3);
+
+console.log(findLargestNum(5, -3, 16));
+console.log(findLargestNum(-3, -5, -22.5));
