7cffde2d87b2929b9bd69f662a66f1b86d9aa1ea
pkindalov <panayotkindalov@gmail.com>
2024-04-02 17:25:44 +0300

    Renamed folder

--- Introduction to TS/src/CalculateRectangleArea/calcRectArea.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-/* Write a simple function that calculates rectangle area. You will be given two integers (whole) numbers,
-   which will represent length and width of the rectangle. Calculate and print the area of the rectangle.
-*/
-const calcRectArea = (length, whidth) => length * whidth;
-calcRectArea(5, 7);
-calcRectArea(6, 8);
-//# sourceMappingURL=calcRectArea.js.map
